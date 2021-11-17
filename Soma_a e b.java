7598ba8989d4fac899d91410e47ab277ea63a3d5
Luiz Felipy Badain <88565656+luiz4513@users.noreply.github.com>
2021-11-17 12:32:40 -0300

    Add files via upload

---
+++ Soma_a e b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SOMA {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int a,b, valor;
+        
+        System.out.println("Vamos lá!!");
+        System.out.println("Coloque o valor de a:");
+        a= in.nextInt();
+        System.out.println("coloque agora, o valor de b:");
+        b=in.nextInt();
+        
+         valor= a + b;
+         
+         if(a == b){
+        System.out.println(" a soma de a e b é: "  + valor);
+        } else{
+         
+    }
+   }
+}
+     
